259011b34af39dc36572abe535fcfa28909951e2
Viktor <swifrorlilko@gmail.com>
2020-01-30 21:02:09 +0200

    adding error handle

--- maczvado/mac_change.py
+++ maczvado/mac_change.py
@@ -5,7 +5,12 @@ def getArgs():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Specify interface to change MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="Input new mac address")
-    return parser.parse_args()
+    (opt, arg) = parser.parse_args()
+    if not opt.interface:
+        parser.error("[-] Please specify an interface. Use --help for information.")
+    elif not op.new_mac:
+        parser.error("[-] Please specify a new mac address. Use --help for information.")
+    return opt
 
 def changeMac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
@@ -16,6 +21,6 @@ def changeMac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
     print("[+] interface up MAC changed")
 
-(opt, args) = getArgs()
+opt = getArgs()
 
 changeMac(opt.interface, opt.new_mac)
