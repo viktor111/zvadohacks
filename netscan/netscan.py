f70f4f724f239c275459ef671f3984ae6154f7c2
Viktor <swifrorlilko@gmail.com>
2020-01-31 18:13:45 +0200

    setting timeout and summary

--- netscan/netscan.py
+++ netscan/netscan.py
@@ -2,7 +2,10 @@ import scapy.all as scapy
 
 def scan(ip):
     arpRequest = scapy.ARP(pdst=ip)
-    print(arpRequest.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arpRequestBroadcast = broadcast/arpRequest
+    answered, unanswered = scapy.srp(arpRequestBroadcast, timeout=1)
+    print(answered.summary())
 
-scan("192.168.0.1")
+scan("192.168.0.1/24")
 
