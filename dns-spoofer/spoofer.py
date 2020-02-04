bc18e09164ea589884c48729bcc69d3662c071ba
Viktor <swifrorlilko@gmail.com>
2020-02-04 14:45:48 +0200

    adding args and queue

--- dns-spoofer/spoofer.py
+++ dns-spoofer/spoofer.py
@@ -3,3 +3,20 @@ import scapy
 import optparse
 import netfilterqueue
 
+
+def getArgs():
+    parser = optparse.OptionParser()
+    parser.add_option("-q", "--queue", dest="queueNum", help="Specify queue number.")
+    (opt, arg) = parser.parse_args()
+    if not opt.queueNum:
+        parser.error("[-] Plase specify queue number. Use --help for more information.")
+
+    return opt
+
+def createQueue(queueNum):
+    subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", queueNum])
+    print('Queue created with number -{queueNum}-')
+
+opt = getArgs()
+# createQueue(opt.queueNum)
+
