0e0d36b34fbf7b705add841ab71f838243ff7e81
Viktor <swifrorlilko@gmail.com>
2020-02-05 14:21:38 +0200

    add test option

--- dns-spoof/spoofer.py
+++ dns-spoof/spoofer.py
@@ -6,26 +6,36 @@ import optparse
 def getArgs():
     parser = optparse.OptionParser()
     parser.add_option("-q", "--queue", dest="queueNum", help="Specify queue number.")
+    parser.add_option("-t", "--test", dest="test", help="Test only on your machine 0 for no 1 for yes.")
     (opt, arg) = parser.parse_args()
     if not opt.queueNum:
         parser.error("[-] Plase specify queue number. Use --help for more information.")
-
+    if not opt.test:
+        parser.error("[-] Plase specify test option. Use --help for more information.")
     return opt
 
 
-def createQueue(queueNum):
-    subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", queueNum])
-    print("Queue created")
+def createQueue(queueNum, test):
+    if test == 0:
+        subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", queueNum])
+        print("Queue created!")
+    if test == 1:
+        subprocess.call(["iptables", "-I", "OUTPUT", "-j", "NFQUEUE", "--queue-num", queueNum])
+        subprocess.call(["iptables", "-I", "INPUT", "-j", "NFQUEUE", "--queue-num", queueNum])
+        print("Test queue created!")
+
+
 
 
 def processPacket(packet):
     scapyPacket = scapy.IP(packet.get_payload())
-    print(scapyPacket.show())
+    if scapyPacket.haslayer(scapy.DNSRR):
+        print(scapyPacket.show())
     packet.accept()
 
 
 opt = getArgs()
-createQueue(opt.queueNum)
+createQueue(opt.queueNum, int(opt.test))
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(int(opt.queueNum), processPacket)
 queue.run()
