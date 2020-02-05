624d964f114144258586c4ceb75c52596e98375a
Viktor <swifrorlilko@gmail.com>
2020-02-05 13:13:20 +0200

    adding queue and user arg

--- dns-spoof/spoofer.py
+++ dns-spoof/spoofer.py
@@ -1,8 +1,31 @@
 import netfilterqueue
+import scapy
+import subprocess
+import optparse
+
+def getArgs():
+    parser = optparse.OptionParser()
+    parser.add_option("-q", "--queue", dest="queueNum", help="Specify queue number.")
+    (opt, arg) = parser.parse_args()
+    if not opt.queueNum:
+        parser.error("[-] Plase specify queue number. Use --help for more information.")
+
+    return opt
 
 
-def processPacket():
+def createQueue(queueNum):
+    subprocess.call(["iptables", "-I", "FORWARD", "-j", "NFQUEUE", "--queue-num", queueNum])
+    print("Queue created")
+
+
+def processPacket(packet):
     scapyPacket = scapy.IP(packet.get_payload())
     packet.accept()
 
+
+opt = getArgs()
+createQueue(opt.queueNum)
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(opt.queueNum, processPacket)
+queue.run()
 
