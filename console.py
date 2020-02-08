26eedaa5d1a3d50f5ba905155c7f707c33027568
Viktor <swifrorlilko@gmail.com>
2020-02-08 13:51:20 +0200

    user input add

--- console.py
+++ console.py
@@ -1,9 +1,23 @@
 import pyfiglet
+import sys
+sys.path.append("./")
+import change_mac
+
 
 banner = pyfiglet.figlet_format("Hello HACKERS!")
 print(banner)
 
-while True:
+tools = ["change_mac", " net_spoof", "net_scan", "dns_spoof", "sniffer", "download_intercept"]
 
-    user_input = input()
+def user_input():
+    command = input("->")
+    if command == "exit":
+        print("bye")
+        sys.exit()
+    if "run" in command:
+        run_command = command.split()
+        tool = run_command[1]
+
+while True:
+    user_input()
 
