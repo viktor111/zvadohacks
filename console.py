1b9ef33699129e81cc71781d436d520381a9d780
Viktor <swifrorlilko@gmail.com>
2020-02-09 23:42:40 +0200

    adding help

--- console.py
+++ console.py
@@ -1,4 +1,5 @@
 import pyfiglet
+import mac_change
 import sys
 sys.path.append("./")
 
@@ -15,19 +16,24 @@ def user_input():
     if "run" in command:
         try:
             run_command = command.split()
+            print(run_command)
             tool = run_command[1]
             options = run_command[2]
         except IndexError:
-            print("[-] Please input command with options")
+            print("[-] Please input command run command with options or check if the tool name is correct.")
             user_input()
         if tool not in tools:
             print("Pease insert correct tool name!")
         if tool in tools:
             if tool == "change_mac":
-                from change_mac import mac_change
-                def run():
-                    mac_change
-                run()
+                print("whYYYYYYYYYYYYYYYYYYYYYYYY")
+                interface = run_command[3]
+                new_mac = run_command[5]
+                print(interface)
+                print(new_mac)
+                mac_change.changeMac(interface, new_mac)
+    if "help" in command:
+        print("this is help")
 
 while True:
     user_input()
